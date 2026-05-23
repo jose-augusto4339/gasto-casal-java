e554038c64bab86d41daff3f43d8244b440fa4d9
Jose Augusto <joseaugustoribeiro1998@gmail.com>
2026-05-23 17:20:48 -0300

    primeiro commit

---
+++ src/main/java/com/br/jos/gasto_casal/GastoCasalApplication.java
@@ -0,0 +1,13 @@
+package com.br.jos.gasto_casal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastoCasalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastoCasalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/jos/gasto_casal/GastoCasalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.jos.gasto_casal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastoCasalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
